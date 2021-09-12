d2c5380b9a0f68a4f048dcf0238252a0eb0e4fce
Blue <leanefelis@gmail.com>
2021-09-12 18:10:36 +0200

    Méthodes finalisées + code aéré + affichage ok

--- Projet/src/Revue.java
+++ Projet/src/Revue.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Revue {
 
-    // Connexion déjà établie, je commente les lignes
+    // Connexion déjà établie, je commente les lignes ?
     /* private Connection laConnexion;
     public Revue() {
         Connexion bdd = new Connexion();
@@ -17,12 +17,26 @@ public class Revue {
         try {
             //Connection laConnexion = creeConnexion();
             PreparedStatement requete = laConnexion.prepareStatement("INSERT INTO Periodicite(libelle) VALUES(?)");
+            System.out.println("Veuillez entrer :\n");
+
+            System.out.println("L'identifiant de la revue : ");
             requete.setInt(1,id);
+
+            System.out.println("Le titre : ");
             requete.setString(2,titre);
+
+            System.out.println("La description : ");
             requete.setString(3, description);
+
+            System.out.println("Le tarif : ");
             requete.setFloat(4, tarif);
+
+            System.out.println("Le visuel : ");
             requete.setString(5, visuel);
+
+            System.out.println("La périodicité : ");
             requete.setInt(6, periodicite);
+
             Integer res = requete.executeUpdate();
             System.out.println("Ajouté avec succès");
 
@@ -39,7 +53,10 @@ public class Revue {
 
         try {
             PreparedStatement requete = laConnexion.prepareStatement("DELETE FROM Revue WHERE id_revue = ?");
+            
+            System.out.println("Veuillez entre l'identifiant de la revue : ");
             requete.setInt(1,idRevue);
+
             int res = requete.executeUpdate();
             if (res == 1) {
                 System.out.println("Elément supprimé avec succès !");
@@ -58,19 +75,35 @@ public class Revue {
 
 
     /* Méthode d'édition d'une Revue */
-    public void edit(int idPeriodicite, String libelle) {
+    public void edit(int idRevue, String titre, String description, float tarif, String visuel) {
 
         try {
-            PreparedStatement requete = laConnexion.prepareStatement("UPDATE Periodicite SET libelle = ? WHERE id_periodicite = ?");
-            requete.setString(1,libelle);
-            requete.setInt(2,idPeriodicite);
+            PreparedStatement requete = laConnexion.prepareStatement("UPDATE Revue SET titre = ?, description = ?, tarif_numero = ?, visuel = ?, WHERE id_periodicite = ?");
+            
+            System.out.println("Veuillez entre l'identifiant de la revue : ");
+            requete.setInt(1,idRevue);
+
+            System.out.println("Veuillez entrer : ");
+            
+            System.out.println("Le titre : ");
+            requete.setString(2,titre);
+
+            System.out.println("La description : ");
+            requete.setString(3,description);
+
+            System.out.println("Le tarif : ");
+            requete.setFloat(4, tarif);
+
+            System.out.println("Le visuel : ");
+            requete.setString(5, visuel);
+
             int res = requete.executeUpdate();
             System.out.println("Modifié avec succès");
 
 
         } catch (SQLException sqle) {
 
-            System.out.println("Problème d'édition d'une périodicité\n" + sqle.getMessage());
+            System.out.println("Problème d'édition d'une Revue\n" + sqle.getMessage());
 
         }
     }
