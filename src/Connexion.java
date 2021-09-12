ab36e460b6a2b09631300d14ae5963e3de3d485a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-09-12 22:04:47 +0200

    Wups j'avais modifier connexion, my bad

--- src/Connexion.java
+++ src/Connexion.java
@@ -1,22 +1,17 @@
 import java.sql.*;
-
-public class Connexion
-{
-    public Connection creeConnexion()
-    {
-        String url =
-                "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/nataneli1u_td_cpoa";
+public class Connexion {
+    public Connection creeConnexion() {
+        String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/muller851u_td_java";
         url += "?serverTimezone=Europe/Paris";
-        String login = "nataneli1u_appli";
-        String pwd = "Xbb8R2D2";
+        String login = "muller851u_appli";
+        String pwd = "Riquzo_8";
         Connection maConnexion = null;
 
-        try
+        try 
         {
             maConnexion = DriverManager.getConnection(url, login, pwd);
-        } catch (SQLException sqle)
-        {
-            System.out.println("Erreur connexion\n" + sqle.getMessage());
+        } catch (SQLException sqle) {
+            System.out.println("Erreur connexion \n" + sqle.getMessage());
         }
         return maConnexion;
     }
