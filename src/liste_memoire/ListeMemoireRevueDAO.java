a58ad2cb39a60c63b85935c0f3ad3722d755bc43
Blue <leanefelis@gmail.com>
2021-09-13 16:35:05 +0200

    ListeMemoireRevueDAO.java terminé

---
+++ src/liste_memoire/ListeMemoireRevueDAO.java
@@ -0,0 +1,110 @@
+package dao.listememoire;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.RevueDAO;
+import modele.Revue;
+
+public class ListeMemoireRevueDAO implements RevueDAO {
+
+	private static ListeMemoireRevueDAO instance;
+
+	private List<Revue> donnees;
+
+
+	public static ListeMemoireRevueDAO getInstance() {
+
+		if (instance == null) {
+			instance = new ListeMemoireRevueDAO();
+		}
+
+		return instance;
+	}
+
+	private ListeMemoireRevueDAO() {
+
+		this.donnees = new ArrayList<Revue>();
+
+		this.donnees.add(new Revue
+                                    (
+                                        1, 
+                                        "Random Revue", 
+                                        "Revue super random", 
+                                        5.7, 
+                                        "Visuel Random pour une revue random",
+                                        1
+                                    ));
+		this.donnees.add(new Revue
+                                    (
+                                        2, 
+                                        "Le Quotidien Aléatoire",
+                                        "Une revue questionnant l'aléatoire",
+                                        10.6,
+                                        "Visuel d'un quotidien aléatoire",
+                                        2
+                                    ));
+	}
+
+
+	@Override
+	public boolean create(Revue objet) {
+
+		objet.setId(3);
+		// Ne fonctionne que si l'objet métier est bien fait...
+		while (this.donnees.contains(objet)) {
+
+			objet.setId(objet.getId() + 1);
+		}
+		boolean ok = this.donnees.add(objet);
+		
+		return ok;
+	}
+
+	@Override
+	public boolean update(Revue objet) {
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de modification d'un objet inexistant");
+		} else {
+			
+			this.donnees.set(idx, objet);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public boolean delete(Revue objet) {
+
+		Revue supprime;
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de suppression d'un objet inexistant");
+		} else {
+			supprime = this.donnees.remove(idx);
+		}
+		
+		return objet.equals(supprime);
+	}
+
+	@Override
+	public Revue getById(int id) {
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(new Revue(id, "test"));
+		if (idx == -1) {
+			throw new IllegalArgumentException("Aucun objet ne possède cet identifiant");
+		} else {
+			return this.donnees.get(idx);
+		}
+	}
+
+	@Override
+	public ArrayList<Revue> findAll() {
+		return (ArrayList<Revue>) this.donnees;
+	}
+}
