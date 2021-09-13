8bd5c19c277040fcbabdc20ebdc8be031f717311
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-09-13 15:29:22 +0200

    Add files via upload

--- src/metier/Abonnement.java
+++ src/metier/Abonnement.java
@@ -7,10 +7,73 @@ public class Abonnement
     int id, idClient, idRevue;
     Date dateDebut, dateFin;
 
-    public Abonnement(Date dateDebut, Date dateFin, )
+    public Abonnement(Date dateDebut, Date dateFin, int idClient, int idRevue)
     {
-
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.idClient = idClient;
+        this.idRevue = idRevue;
     }
 
+    public Abonnement(int id, Date dateDebut, Date dateFin, int idClient, int idRevue)
+    {
+        this.id = id;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.idClient = idClient;
+        this.idRevue = idRevue;
+    }
+
+/******************GETTERS AND SETTERS*******************/
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getIdClient()
+    {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient)
+    {
+        this.idClient = idClient;
+    }
+
+    public int getIdRevue()
+    {
+        return idRevue;
+    }
+
+    public void setIdRevue(int idRevue)
+    {
+        this.idRevue = idRevue;
+    }
+
+    public Date getDateDebut()
+    {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut)
+    {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin()
+    {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin)
+    {
+        this.dateFin = dateFin;
+    }
+/******************************************************/
 
 }
---
+++ src/metier/Periodicite.java
@@ -0,0 +1,38 @@
+package metier;
+
+public class Periodicite
+{
+    int id;
+    String libelle;
+
+    public Periodicite(String libelle)
+    {
+        this.libelle = libelle;
+    }
+
+    public Periodicite(int id, String libelle)
+    {
+           this.id = id;
+           this.libelle = libelle;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getLibelle()
+    {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle)
+    {
+        this.libelle = libelle;
+    }
+}
