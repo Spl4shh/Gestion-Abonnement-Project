3f75a43875e00d76badcdc652892192375d8f227
Blue <leanefelis@gmail.com>
2021-09-13 16:36:18 +0200

    Connexion Romain ajoutée

--- src/Connexion.java
+++ src/Connexion.java
@@ -7,6 +7,8 @@ public class Connexion {
         String pwd = "Riquzo_8";
         Connection maConnexion = null;
 
+//String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/nataneli1u_td_cpoa"; url += "?serverTimezone=Europe/Paris"; String login = "nataneli1u_appli"; String pwd = "Xbb8R2D2"; Connection maConnexion = null;
+
         try 
         {
             maConnexion = DriverManager.getConnection(url, login, pwd);
