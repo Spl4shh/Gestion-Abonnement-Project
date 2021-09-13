8fdeb937764c50542a3ec5c8722bcef720b9e9f8
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-09-13 17:25:16 +0200

    Add files via upload

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,9 +1,14 @@
 package dao;
 
+import metier.Abonnement;
+
+import java.util.ArrayList;
+
 public interface DAO<T>
 {
     public boolean create(T objet);
     public boolean update(T objet);
     public boolean delete(T objet);
     public T getById(int i);
+    public ArrayList<T> findAll();
 }
---
+++ src/dao/listememoire/ListeMemoireAbonnementDAO.java
@@ -0,0 +1,150 @@
+package dao.listememoire;
+
+import dao.AbonnementDAO;
+import metier.Abonnement;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListeMemoireAbonnementDAO implements AbonnementDAO {
+
+    private static ListeMemoireAbonnementDAO instance;
+
+    private List<Abonnement> donnees;
+
+
+    public static ListeMemoireAbonnementDAO getInstance()
+    {
+        if (instance == null) {
+            instance = new ListeMemoireAbonnementDAO();
+        }
+
+        return instance;
+    }
+
+    private ListeMemoireAbonnementDAO()
+    {
+        this.donnees = new ArrayList<Abonnement>();
+
+        DateTimeFormatter formatage = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        java.sql.Date dateDebutSql1 = java.sql.Date.valueOf(LocalDate.parse("23/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        java.sql.Date dateFinSql1 = java.sql.Date.valueOf(LocalDate.parse("24/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+
+        this.donnees.add(new Abonnement(1, java.sql.Date.valueOf(LocalDate.parse("23/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
+                java.sql.Date.valueOf(LocalDate.parse("24/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy"))), 1, 1));
+        this.donnees.add(new Abonnement(2, java.sql.Date.valueOf(LocalDate.parse("23/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
+                java.sql.Date.valueOf(LocalDate.parse("24/04/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy"))), 2, 2));
+    }
+
+
+    @Override
+    public boolean create(Abonnement objet)
+    {
+        objet.setId(3);
+        // Ne fonctionne que si l'objet métier est bien fait...
+        while (this.donnees.contains(objet))
+        {
+            objet.setId(objet.getId() + 1);
+        }
+        boolean ok = this.donnees.add(objet);
+
+        return ok;
+    }
+
+    @Override
+    public boolean update(Abonnement objet)
+    {
+        // Ne fonctionne que si l'objet métier est bien fait...
+        int idx = this.donnees.indexOf(objet);
+        if (idx == -1) {
+            throw new IllegalArgumentException("Tentative de modification d'un objet inexistant");
+        } else {
+
+            this.donnees.set(idx, objet);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean delete(Abonnement objet)
+    {
+        Abonnement supprime;
+
+        // Ne fonctionne que si l'objet métier est bien fait...
+        int idx = this.donnees.indexOf(objet);
+        if (idx == -1)
+        {
+            throw new IllegalArgumentException("Tentative de suppression d'un objet inexistant");
+        } else
+        {
+            supprime = this.donnees.remove(idx);
+        }
+
+        return objet.equals(supprime);
+    }
+
+    @Override
+    public Abonnement getById(int id)
+    {
+        // Ne fonctionne que si l'objet métier est bien fait...
+        int idx = this.donnees.indexOf(new  Abonnement(id, java.sql.Date.valueOf(LocalDate.parse("11/11/1111", DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
+                java.sql.Date.valueOf(LocalDate.parse("11/11/1111", DateTimeFormatter.ofPattern("dd/MM/yyyy"))), 0, 0));
+        if (idx == -1)
+        {
+            throw new IllegalArgumentException("Aucun objet ne possède cet identifiant");
+        } else
+        {
+            return this.donnees.get(idx);
+        }
+    }
+
+    @Override
+    public ArrayList<Abonnement> findAll()
+    {
+        return (ArrayList<Abonnement>) this.donnees;
+    }
+
+    @Override
+    public List<Abonnement> getByDate(Date dateDebut, Date dateFin)
+    {
+        List<Abonnement> listAbonnement = null;
+        /*
+        *   Utiliser la methode equals qui seras a surcharger
+        */
+        // Ne fonctionne que si l'objet métier est bien fait...
+        int idx = this.donnees.indexOf(new  Abonnement(0, dateDebut, dateFin, 0, 0));
+        if (listAbonnement.isEmpty())
+        {
+            throw new IllegalArgumentException("Aucun objet ne possède ce nom");
+        } else
+        {
+            return listAbonnement;
+        }
+    }
+
+    @Override
+    public List<Abonnement> getByNomPrenom(String nom, String prenom)
+    {
+        List<Abonnement> listAbonnement = null;
+        /*
+         * Recuperer tout les clients ayant ce nom et ensuite affecter les id pour la recherche d'abonnements
+         *   Utiliser la methode equals qui seras a surcharger
+         */
+        // Ne fonctionne que si l'objet métier est bien fait...
+        int idx = this.donnees.indexOf(new  Abonnement(0, java.sql.Date.valueOf(LocalDate.parse("11/11/1111", DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
+                java.sql.Date.valueOf(LocalDate.parse("11/11/1111", DateTimeFormatter.ofPattern("dd/MM/yyyy"))), 0, 0));
+        if (listAbonnement.isEmpty())
+        {
+            throw new IllegalArgumentException("Aucun objet ne possède ce nom");
+        } else
+        {
+            return listAbonnement;
+        }
+    }
+}
