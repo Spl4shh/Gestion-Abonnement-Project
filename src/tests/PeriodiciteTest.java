bb95a6de2848e1d45dbf8a9ae5700d1d53f7f13a
Blue <leanefelis@gmail.com>
2021-09-27 16:15:58 +0200

    efhjzergr

--- src/tests/PeriodiciteTest.java
+++ src/tests/PeriodiciteTest.java
@@ -1,14 +1,17 @@
 package tests;
 
+import metier.Periodicite;
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
-public class PeriodiciteTest
-{
-    @Test 
+public class PeriodiciteTest {
+    @Test
     public void test() {
-        assertEquals();
+        String obj1 = "Junit";
+        String obj2 = "Junit";
+        assertEquals(obj1, obj2);
+
+        assertEquals(5, Periodicite.getId());
     }
 
 }
