d2d4f2ac2cdb451b3877e85ebd8f787eff08a0f4
Blue <leanefelis@gmail.com>
2021-09-27 14:57:46 +0200

    test periodicite

---
+++ src/tests/PeriodiciteTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PeriodiciteTest
+{
+    @Test 
+    public void test() {
+        assertEquals();
+    }
+
+}
