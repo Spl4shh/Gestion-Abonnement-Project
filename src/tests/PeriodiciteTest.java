f8d64e393b08f7e73d4773b7fc80999a8b62d401
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-09-27 16:14:41 +0200

    add test

--- src/tests/PeriodiciteTest.java
+++ src/tests/PeriodiciteTest.java
@@ -1,14 +1,15 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+
+
 
 import org.junit.Test;
 
 public class PeriodiciteTest
 {
     @Test 
-    public void test() {
-        assertEquals();
+    public void test() 
+    {
     }
 
 }
---
+++ src/tests/RevueTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RevueTest
+{
+
+    @Test
+    public void testVrai()
+    {
+        assertEquals(1, 1);
+    }
+}
