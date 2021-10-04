2cc801116657a5c225dad569d6ee96e805e8cf1d
Blue <leanefelis@gmail.com>
2021-10-04 15:03:28 +0200

    Modifs

--- src/metier/ProcessAdress.java
+++ src/metier/ProcessAdress.java
@@ -5,7 +5,6 @@ import com.mysql.cj.util.StringUtils;
 public class ProcessAdress 
 {
     // Normalisation Adresse
-
     public Adresse normalizeAdresse(Adresse adresse)
     {
         Adresse adresseNormalise;
@@ -14,30 +13,29 @@ public class ProcessAdress
     }
 
     // Normalisation du Pays
-
     public String normalizePays(String pays)
     {
-        String paysMinus = pays.toLowerCase();
-
-        switch (paysMinus) 
-        {
-            case "letzebuerg":
-                pays = "Luxembourg";
-                break;
-
-            case "belgium":
-                pays = "Belgique";
-                break;
-
-            case "switzerland", "schweiz":
-                pays = "Suisse";
-                break;
+        if (pays != null) { // Si la chaine pays n'est pas vide
+            String paysMinus = pays.trim().toLowerCase(); // Chaine pays nettoyée et en minuscules
+            switch (paysMinus) 
+            {
+                case "letzebuerg":
+                    pays = "Luxembourg";
+                    break;
+    
+                case "belgium":
+                    pays = "Belgique";
+                    break;
+    
+                case "switzerland", "schweiz":
+                    pays = "Suisse";
+                    break;
+            }
         }
         return pays;
     }
 
     // Normalisation Voie
-
     public String normalizeVoie(String voie) 
     {
         return (", " + voie);
