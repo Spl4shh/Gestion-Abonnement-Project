54f1774c4fdf14c22ead42a3adbfc4ec970e7014
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-04 14:27:03 +0200

    Update Client.java

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -3,29 +3,22 @@ package metier;
 public class Client 
 {
     int id;
-    String nom, prenom, noRue, voie, codePostal, ville, pays;
+    String nom, prenom;
+    Adresse adresse;
 
-    public Client(String nom, String prenom, String noRue, String voie, String codePostal, String ville, String pays) 
+    public Client(String nom, String prenom, Adresse adresse) 
     {
         this.nom = nom;
         this.prenom = prenom;
-        this.noRue = noRue;
-        this.voie = voie;
-        this.codePostal = codePostal;
-        this.ville = ville;
-        this.pays = pays;
+        this.adresse = adresse;
     }
 
-    public Client(int id, String nom, String prenom, String noRue, String voie, String codePostal, String ville, String pays) 
+    public Client(int id, String nom, String prenom, Adresse adresse) 
     {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
-        this.noRue = noRue;
-        this.voie = voie;
-        this.codePostal = codePostal;
-        this.ville = ville;
-        this.pays = pays;
+        this.adresse = adresse;
     }
 
     public Client(int id) 
@@ -63,56 +56,6 @@ public class Client
         this.nom = nom;
     }
 
-    public String getNoRue() 
-    {
-        return this.noRue;
-    }
-
-    public void setNoRue(String noRue) 
-    {
-        this.noRue = noRue;
-    }
-
-    public String getVoie() 
-    {
-        return this.voie;
-    }
-
-    public void setVoie(String voie) 
-    {
-        this.voie = voie;
-    }
-
-    public String getCodePostal() 
-    {
-        return this.codePostal;
-    }
-
-    public void setCodePostal(String codePostal) 
-    {
-        this.codePostal = codePostal;
-    }
-
-    public String getVille() 
-    {
-        return this.ville;
-    }
-
-    public void setVille(String ville) 
-    {
-        this.ville = ville;
-    }
-
-    public String getPays() 
-    {
-        return this.pays;
-    }
-
-    public void setPays(String pays) 
-    {
-        this.pays = pays;
-    }
-
     @Override
     public boolean equals(Object object)
     {
@@ -121,11 +64,7 @@ public class Client
         if (this.id == client.id &&
             this.nom.equals(client.nom) &&
             this.prenom.equals(client.prenom) &&
-            this.noRue.equals(client.noRue) &&
-            this.voie.equals(client.voie) &&
-            this.codePostal.equals(client.codePostal) &&
-            this.ville.equals(client.ville) &&
-            this.pays.equals(client.pays)) 
+            this.adresse.equals(client.adresse)) 
         {
             return true;
         }
@@ -138,6 +77,6 @@ public class Client
     @Override
     public String toString()
     {
-        return (this.getId() + " " + this.getNom() + " " + this.getPrenom() + " " + this.getNoRue() + " " + this.getVoie() + " " + this.getCodePostal() + " " + this.getVille() + " " + this.getPays());
+        return (this.getId() + " " + this.getNom() + " " + this.getPrenom() + " " + this.adresse.toString());
     }
 }
