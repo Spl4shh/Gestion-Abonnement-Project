f414091ff3ac1492247b74e947c9128715537228
Blue <leanefelis@gmail.com>
2021-10-04 14:27:05 +0200

    getters setters

---
+++ src/metier/Adresse.java
@@ -0,0 +1,57 @@
+package metier;
+
+public class Adresse {
+    // Setters / getters
+
+    private String noRue, voie, codePostal, ville, pays;
+
+    public String getNoRue() 
+    {
+        return this.noRue;
+    }
+
+    public void setNoRue(String noRue) 
+    {
+        this.noRue = noRue;
+    }
+
+    public String getVoie() 
+    {
+        return this.voie;
+    }
+
+    public void setVoie(String voie) 
+    {
+        this.voie = voie;
+    }
+
+    public String getCodePostal() 
+    {
+        return this.codePostal;
+    }
+
+    public void setCodePostal(String codePostal) 
+    {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() 
+    {
+        return this.ville;
+    }
+
+    public void setVille(String ville) 
+    {
+        this.ville = ville;
+    }
+
+    public String getPays() 
+    {
+        return this.pays;
+    }
+
+    public void setPays(String pays) 
+    {
+        this.pays = pays;
+    }
+}
