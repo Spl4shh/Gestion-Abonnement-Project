7741466207a8d1c9aaac42866e0b6a7740fe8a51
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-07 20:34:57 +0200

    Preparation class normalizationCodeTest

---
+++ src/tests/ProcessAdressTest/normalizationCodeTest.java
@@ -0,0 +1,44 @@
+package tests.ProcessAdressTest;
+
+import org.junit.Test;
+
+public class normalizationCodeTest 
+{
+    
+
+    @Test
+    public void testChaineNull()
+    {
+
+    }
+
+    @Test
+    public void testChaineVide()
+    {
+
+    }
+
+    @Test
+    public void testChaine5Chiffres()
+    {
+
+    }
+
+    @Test
+    public void testChaine4Chiffres()
+    {
+
+    }
+
+    @Test
+    public void testChaine4ChiffresIdPays()
+    {
+
+    }
+
+    @Test
+    public void testChaine5ChiffresIdPays()
+    {
+
+    }
+}
