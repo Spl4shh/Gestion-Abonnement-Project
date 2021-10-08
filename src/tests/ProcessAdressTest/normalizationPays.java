8228a2407d43029bf00aa1e77d145def3c1d4450
Blue <leanefelis@gmail.com>
2021-10-08 17:19:45 +0200

    test chaine null pays

--- src/tests/ProcessAdressTest/normalizationPays.java
+++ src/tests/ProcessAdressTest/normalizationPays.java
@@ -1,15 +1,20 @@
 package tests.ProcessAdressTest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import metier.ProcessAdress;
 
 public class normalizationPays 
 {
-    
+    String res;
 
     @Test
     public void testChaineNull()
     {
-
+        res = ProcessAdress.normalizePays(null);
+        assertEquals(null, res);
     }
 
     @Test
