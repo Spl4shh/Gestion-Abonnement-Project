9f6057d22d0d3f640e3c18a26ebe045a0db40a5a
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-08 17:50:59 +0200

    Update normalizationCodePostalTest.java

--- src/tests/ProcessAdressTest/normalizationCodePostalTest.java
+++ src/tests/ProcessAdressTest/normalizationCodePostalTest.java
@@ -6,48 +6,49 @@ import org.junit.Test;
 
 import metier.ProcessAdress;
 
-public class normalizationCodePostalTest {
+public class normalizationCodePostalTest 
+{
     String res;
 
     @Test
     public void testChaineVide()
     {
-        res = ProcessAdress.normalizeCode2("");
+        res = ProcessAdress.normalizeCode("");
         assertEquals("", res);
     }
 
     @Test
     public void testChaineNull()
     {
-        res = ProcessAdress.normalizeCode2(null);
+        res = ProcessAdress.normalizeCode(null);
         assertEquals(null, res);
     }
 
     @Test
     public void testChaineNombre5()
     {
-        res = ProcessAdress.normalizeCode2("57000");
+        res = ProcessAdress.normalizeCode("57000");
         assertEquals("57000", res);
     }
 
     @Test
     public void testChaineNombre4IdPays()
     {
-        res = ProcessAdress.normalizeCode2("L-7420");
+        res = ProcessAdress.normalizeCode("L-7420");
         assertEquals("07420", res);
     }
 
     @Test
     public void testChaineNombre5IdPays()
     {
-        res = ProcessAdress.normalizeCode2("L-57420");
+        res = ProcessAdress.normalizeCode("L-57420");
         assertEquals("57420", res);
     }
 
     @Test
     public void testChaineLettre()
     {
-        res = ProcessAdress.normalizeCode2("ABCDE");
+        res = ProcessAdress.normalizeCode("ABCDE");
         assertEquals("", res);
     }
 }
