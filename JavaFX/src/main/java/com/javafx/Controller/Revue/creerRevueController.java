002bbf024b8999379b1fc8b42cdacf4a5feecdd5
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-10 14:27:08 +0200

    Valide

--- JavaFX/src/main/java/com/javafx/Controller/Revue/creerRevueController.java
+++ JavaFX/src/main/java/com/javafx/Controller/Revue/creerRevueController.java
@@ -1,30 +1,55 @@
 package com.javafx.Controller.Revue;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class creerRevueController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class creerRevueController implements Initializable
 {
     @FXML
     private Label affichageLabel;
-    @FXML
-    private TextField titreField = new TextField();
-    @FXML
-    private TextArea descriptionArea = new TextArea();
-    @FXML
-    private TextField tarifField = new TextField();
-    @FXML
-    private ComboBox periodiciteComboBox = new ComboBox();
-
 
     @FXML
-    protected void boutonCreerRevueClick()
+    private Button boutonCreer;
+
+    @FXML
+    private TextArea descriptionArea;
+
+    @FXML
+    private TextField tarifField;
+
+    @FXML
+    private TextField titreField;
+
+    @FXML
+    private ChoiceBox periodiciteChoiceBox;
+
+    @FXML
+    void boutonCreerRevueClick(ActionEvent event)
     {
+        String titre = titreField.getText();
+        String tarif = tarifField.getText();
+        String description = descriptionArea.getText();
+        int idPeriodicite = (int)periodiciteChoiceBox.getValue();
 
-
-        affichageLabel.setText(titreField.getText());
+        affichageLabel.setText(String.valueOf(idPeriodicite));
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+/*
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
+        this.cbxPeriodicite.setItems(FXCollections.observableArrayList(dao.getPeriodiciteDAO().findAll()));
+    }
+    */
+
 }
