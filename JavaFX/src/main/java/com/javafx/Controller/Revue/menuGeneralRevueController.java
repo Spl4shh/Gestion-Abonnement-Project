86f70e6252ea9c42a79c85359e02efd99f49c2bd
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-12 20:37:35 +0200

    Update menuGeneralRevueController.java

--- JavaFX/src/main/java/com/javafx/Controller/Revue/menuGeneralRevueController.java
+++ JavaFX/src/main/java/com/javafx/Controller/Revue/menuGeneralRevueController.java
@@ -13,44 +13,51 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.scene.control.ListView;
+import org.w3c.dom.events.MouseEvent;
 
 public class menuGeneralRevueController implements Initializable
 {
-        @FXML
-        private ListView<Revue> revueListView;
+    @FXML
+    private ListView<Revue> revueListView;
 
-        @FXML
-        private TableView<Revue> tableViewRevue;
+    @FXML
+    private TableView<Revue> tableViewRevue;
 
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle)
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
+
+        TableColumn<Revue, Integer> colId =  new TableColumn<>("ID");
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Revue, String> colTitre =  new TableColumn<>("Titre");
+        colTitre.setCellValueFactory(new PropertyValueFactory<>("titre"));
+
+        TableColumn<Revue, String> colDescription =  new TableColumn<>("Description");
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        TableColumn<Revue, Double> colTarifUnit =  new TableColumn<>("Tarif Unitaire");
+        colTarifUnit.setCellValueFactory(new PropertyValueFactory<>("tarifNumero"));
+
+        TableColumn<Revue, Integer> colIdPeriodicite =  new TableColumn<>("ID Périodicité");
+        colIdPeriodicite.setCellValueFactory(new PropertyValueFactory<>("idPeriodicite"));
+
+        this.tableViewRevue.getColumns().setAll(colId, colTitre, colDescription, colTarifUnit, colIdPeriodicite);
+
+        try
         {
-            DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
-
-            TableColumn<Revue, Integer> colId =  new TableColumn<>("ID");
-            colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-
-            TableColumn<Revue, String> colTitre =  new TableColumn<>("Titre");
-            colTitre.setCellValueFactory(new PropertyValueFactory<>("titre"));
-
-            TableColumn<Revue, String> colDescription =  new TableColumn<>("Description");
-            colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
-
-            TableColumn<Revue, Double> colTarifUnit =  new TableColumn<>("Tarif Unitaire");
-            colTarifUnit.setCellValueFactory(new PropertyValueFactory<>("tarifNumero"));
-
-            TableColumn<Revue, Integer> colIdPeriodicite =  new TableColumn<>("ID Périodicité");
-            colIdPeriodicite.setCellValueFactory(new PropertyValueFactory<>("idPeriodicite"));
-
-            this.tableViewRevue.getColumns().setAll(colId, colTitre, colDescription, colTarifUnit, colIdPeriodicite);
-
-            try
-            {
-                this.tableViewRevue.getItems().addAll(dao.getRevueDAO().findAll());
-            } catch (SQLException e)
-            {
-                e.printStackTrace();
-            }
+            this.tableViewRevue.getItems().addAll(dao.getRevueDAO().findAll());
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
         }
+    }
+
+    @FXML
+    void listViewOnClick(MouseEvent event)
+    {
+
+    }
 }
 
