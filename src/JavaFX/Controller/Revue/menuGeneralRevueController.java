34c2787c15f33f7fdec85744bfa915ffdbb4d7b8
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-14 21:31:05 +0200

    s

--- src/JavaFX/Controller/Revue/menuGeneralRevueController.java
+++ src/JavaFX/Controller/Revue/menuGeneralRevueController.java
@@ -82,7 +82,7 @@ public class menuGeneralRevueController implements Initializable, ChangeListener
     }
 
     @FXML
-    void btnSupprimerClick(ActionEvent event) throws IOException, SQLException {
+    void btnSupprimerClick(ActionEvent event) throws SQLException {
         Revue revueASupprimer = this.tableViewRevue.getSelectionModel().getSelectedItem();
         /*
         Supprimer la revue de la DAO utilisé
