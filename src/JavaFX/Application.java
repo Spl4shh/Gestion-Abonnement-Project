738a2fdde530e692d7dfa64004ba7ef1d220c1c2
Blue <leanefelis@gmail.com>
2021-10-16 11:09:21 +0200

    modifs

--- src/JavaFX/Application.java
+++ src/JavaFX/Application.java
@@ -11,8 +11,8 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/Abonnement/creerAbonnement.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/Periodicite/menuGeneralPeriodicite.fxml"));
+        // "Vue/Revue/menuGeneralRevue.fxml"
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Gestion de Revue");
         stage.setScene(scene);
