6a255a6ebf816bd30c8a5fb2ecdcabf0c3c41dec
Blue <leanefelis@gmail.com>
2021-10-16 09:52:25 +0200

    modifs

---
+++ src/JavaFX/Controller/Periodicite/MenuGeneralPeriodiciteController.java
@@ -0,0 +1,44 @@
+package JavaFX.Controller.Periodicite;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MenuGeneralPeriodiciteController {
+
+    @FXML
+    private Button btnAjouter;
+
+    @FXML
+    private Button btnModifier;
+
+    @FXML
+    private Button btnSupprimer;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colIdPeriodicite;
+
+    @FXML
+    private TableView<?> tableViewRevue;
+
+    @FXML
+    void btnAjouterClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnModifierClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupprimerClick(ActionEvent event) {
+
+    }
+
+}
