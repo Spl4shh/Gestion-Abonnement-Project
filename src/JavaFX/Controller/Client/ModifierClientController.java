5a10e484b165beea0c1f4aae7ed30a938a30d15d
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-17 18:17:05 +0200

    s

---
+++ src/JavaFX/Controller/Client/ModifierClientController.java
@@ -0,0 +1,54 @@
+package JavaFX.Controller.Client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ModifierClientController {
+
+    @FXML
+    private Label affichageLabel;
+
+    @FXML
+    private Button boutonAnnuler;
+
+    @FXML
+    private Button boutonModifier;
+
+    @FXML
+    private TextField codepostalField;
+
+    @FXML
+    private Label labelId;
+
+    @FXML
+    private TextField noRueField;
+
+    @FXML
+    private TextField nomField;
+
+    @FXML
+    private TextField paysField;
+
+    @FXML
+    private TextField prenomField;
+
+    @FXML
+    private TextField villeField;
+
+    @FXML
+    private TextField voieField;
+
+    @FXML
+    void boutonAnnulerClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void boutonModifierClientClick(ActionEvent event) {
+
+    }
+
+}
