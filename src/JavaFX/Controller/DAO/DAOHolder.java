62af00163ec96dbc36e59b559497cd4d09acc264
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-17 23:54:26 +0200

    d

--- src/JavaFX/Controller/DAO/DAOHolder.java
+++ src/JavaFX/Controller/DAO/DAOHolder.java
@@ -11,20 +11,27 @@ public class DAOHolder
     private DAOFactory daoFactory;
     private DAO<?> dao;
     private final static DAOHolder INSTANCE = new DAOHolder();
+
     private DAOHolder() {}
+
     public static DAOHolder getInstance()
     {
         return INSTANCE;
     }
-    public DAO<?> getDao() {
+
+    public DAO<?> getDao()
+    {
         return dao;
     }
+
     public DAOFactory getDaoFactory() {
         return daoFactory;
     }
+
     public void setDaoFactory(Persistance persistance) {
         this.daoFactory = DAOFactory.getDAOFactory(persistance);
     }
+
     public void setDao(String table)
     {
         table = table.toLowerCase();
