9651865cb6c73fca7887c00484dd2684b6516b2c
Blue <leanefelis@gmail.com>
2021-10-18 14:44:49 +0200

    annuler ok

--- src/JavaFX/Application.java
+++ src/JavaFX/Application.java
@@ -13,12 +13,17 @@ public class Application extends javafx.application.Application
     @Override
     public void start(Stage stage) throws IOException
     {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/DAO/choixTable.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/Periodicite/menuGeneralPeriodicite.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Gestion de Revue");
         stage.setScene(scene);
         stage.show();
+
+        /*FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/DAO/choixTable.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        stage.setTitle("Gestion de Revue");
+        stage.setScene(scene);
+        stage.show();*/
     }
 
     public static void main(String[] args)
