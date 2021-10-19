912a4dc8e70c39b5182f3101da17d5e7b3389f85
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-19 23:36:53 +0200

    Create AfficherPeriodiciteController.java

---
+++ src/JavaFX/Controller/Periodicite/AfficherPeriodiciteController.java
@@ -0,0 +1,24 @@
+package JavaFX.Controller.Periodicite;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AfficherPeriodiciteController {
+
+    @FXML
+    private Button boutonRetour;
+
+    @FXML
+    private Label labelId;
+
+    @FXML
+    private Label labelLibelle;
+
+    @FXML
+    void boutonRetourClick(ActionEvent event) {
+
+    }
+
+}
