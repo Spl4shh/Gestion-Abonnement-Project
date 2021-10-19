11afbf57e668e7b1ba4892f0196c7f69992083b6
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-19 23:26:22 +0200

    ds

---
+++ src/JavaFX/Controller/Client/AfficherClient.java
@@ -0,0 +1,42 @@
+package JavaFX.Controller.Client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ModifierClientController {
+
+    @FXML
+    private Button boutonRetour;
+
+    @FXML
+    private Label labelCodePostal;
+
+    @FXML
+    private Label labelId;
+
+    @FXML
+    private Label labelNoRue;
+
+    @FXML
+    private Label labelNom;
+
+    @FXML
+    private Label labelPays;
+
+    @FXML
+    private Label labelPrenom;
+
+    @FXML
+    private Label labelVille;
+
+    @FXML
+    private Label labelVoie;
+
+    @FXML
+    void boutonRetourClick(ActionEvent event) {
+
+    }
+
+}
