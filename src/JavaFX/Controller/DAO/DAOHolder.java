de7563b954710d5d8c4fc821dcb9858f04e7fa07
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-10-19 10:18:43 +0200

    Update DAOHolder.java

--- src/JavaFX/Controller/DAO/DAOHolder.java
+++ src/JavaFX/Controller/DAO/DAOHolder.java
@@ -9,7 +9,6 @@ import java.util.Locale;
 public class DAOHolder
 {
     private DAOFactory daoFactory;
-    private DAO<?> dao;
     private final static DAOHolder INSTANCE = new DAOHolder();
 
     private DAOHolder() {}
@@ -17,11 +16,6 @@ public class DAOHolder
     public static DAOHolder getInstance()
     {
         return INSTANCE;
-    }
-
-    public DAO<?> getDao()
-    {
-        return dao;
     }
 
     public DAOFactory getDaoFactory() {
@@ -32,31 +26,4 @@ public class DAOHolder
         this.daoFactory = DAOFactory.getDAOFactory(persistance);
     }
 
-    public void setDao(String table)
-    {
-        table = table.toLowerCase();
-        switch (table)
-        {
-            case "client" :
-            {
-                dao = daoFactory.getClientDAO();
-                break;
-            }
-            case "abonnement" :
-            {
-                dao = daoFactory.getAbonnementDAO();
-                break;
-            }
-            case "periodicite" :
-            {
-                dao = daoFactory.getPeriodiciteDAO();
-                break;
-            }
-            case "revue" :
-            {
-                dao = daoFactory.getRevueDAO();
-                break;
-            }
-        }
-    }
 }
