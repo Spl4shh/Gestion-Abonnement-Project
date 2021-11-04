0314f274e9ff7d02ea7e4596e724ec3547b59a4c
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-11-04 21:06:06 +0100

    Update CreerPeriodiciteController.java

--- src/JavaFX/Controller/Periodicite/CreerPeriodiciteController.java
+++ src/JavaFX/Controller/Periodicite/CreerPeriodiciteController.java
@@ -84,12 +84,15 @@ public class CreerPeriodiciteController
 
     public void returnToMenu() throws IOException
     {
+        //Scene actuelle
+        Scene actualScene = affichageLabel.getScene();
+
         //Charger la page que l'on veux afficher
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Vue/Periodicite/menuGeneralPeriodicite.fxml"));
         //Creer une Scene contenant cette page
-        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        Scene scene = new Scene(fxmlLoader.load(), actualScene.getWidth(), actualScene.getHeight());
         //Recuperer la Stage de l'ancienne page
-        Stage stage = (Stage) affichageLabel.getScene().getWindow();
+        Stage stage = (Stage) actualScene.getWindow();
         //Afficher la nouvelle Scene dans l'ancienne Stage
         stage.setScene(scene);
         stage.setTitle("Menu Périodicité");
